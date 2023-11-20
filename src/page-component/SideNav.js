37fe7fae794e5b307592db1917316b408e734ebf
Skchauhan07 <sudhanshukumar.jnvr@gmail.com>
2023-11-21 04:53:16 +0530

    commit 4

--- src/page-component/SideNav.js
+++ src/page-component/SideNav.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import "../css_files/Sidebar.css"
+import React from "react";
+import "../css_files/Sidebar.css";
+import logo192 from "../../src/iiitb_logo.jpg";
+import my_pic from "../../src/my_pic.jpeg";
 
 const SideNav = () => {
   return (
     <>
-      <div className="sidebar">
-            <li className="sidebartitle">Overview</li>
-            <li className="sidebartitle">Onboarding</li>
+      <div className="container">
+        <img className="logo" src={logo192} />
+        <div className="content">Overview</div>
+        <div className="content">Onboarding</div>
+        <div className="content">Monitoring</div>
+        <div className="content">Flagging</div>
+        {/* <div className="content">Source of Income</div> */}
       </div>
+      <div className="profileSection profileInfo profileImg">
+        <img className="my_pic" src={my_pic}  alt="/" />
+      </div>
+      <div className="profileText">sudhanshu.kumar@iiitb.ac.in</div>
     </>
-  )
-}
+  );
+};
 
-export default SideNav
+export default SideNav;
